5e9aed5d1972df73c3072dcb3facc16df885399b
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2024-11-03 17:26:53 -0300

    feat: creating the logistics for receiving notification of the end of the saga

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/consumer/EventConsumer.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/consumer/EventConsumer.java
@@ -1,5 +1,6 @@
 package br.com.microservices.orchestrated.orderservice.core.consumer;
 
+import br.com.microservices.orchestrated.orderservice.core.service.EventService;
 import br.com.microservices.orchestrated.orderservice.core.utils.JsonUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +14,8 @@ public class EventConsumer {
 
     private final JsonUtil jsonUtil;
 
+    private final EventService eventService;
+
     @KafkaListener(
             groupId = "${spring.kafka.consumer.group-id}",
             topics = "${spring.kafka.topic.notify-ending}"
@@ -22,6 +25,6 @@ public class EventConsumer {
 
         var event = jsonUtil.toEvent(payload);
 
-        log.info(event.toString());
+        eventService.notifyEnding(event);
     }
 }
