e14ee006c2783fdc0bf2d03471ccc08991b44a57
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2024-11-14 20:40:14 -0300

    feat: creating logic rules to rollback updating stock inventory-service

--- inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/service/InventoryService.java
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/service/InventoryService.java
@@ -17,7 +17,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
-import static br.com.microservices.orchestrated.inventoryservice.core.enums.SagaStatus.SUCCESS;
+import static br.com.microservices.orchestrated.inventoryservice.core.enums.SagaStatus.*;
 
 @Slf4j
 @Service
@@ -36,8 +36,10 @@ public class InventoryService {
             checkCurrentValidation(event);
             createOrderInventory(event);
             updateInventory(event.getPayload());
+            handleSuccess(event);
         } catch (Exception ex) {
             log.error("Error trying to update inventory", ex);
+            handleFailCurrentNotExecuted(event, ex.getMessage());
         }
         producer.sendEvent(jsonUtil.toJson(event));
     }
@@ -109,5 +111,37 @@ public class InventoryService {
                 .createdAt(LocalDateTime.now())
                 .build();
         event.addHistory(history);
+    }
+
+    private void handleFailCurrentNotExecuted(Event event, String message) {
+        event.setStatus(ROLLBACK_PENDING);
+        event.setSource(CURRENT_SOURCE);
+        addHistory(event, "Fail to update inventory: ".concat(message));
+    }
+
+    public void rollbackInventory(Event event) {
+        event.setStatus(FAIL);
+        event.setSource(CURRENT_SOURCE);
+        try {
+            returnInventoryToPreviousValues(event);
+            addHistory(event, "Rollback executed for inventory.");
+        } catch (Exception ex) {
+            addHistory(event, "Rollback not executed for inventory: ".concat(ex.getMessage()));
+        }
+        producer.sendEvent(jsonUtil.toJson(event));
+    }
+
+    private void returnInventoryToPreviousValues(Event event) {
+        orderInventoryRepository
+                .findByOrderIdAndTransactionId(event.getOrderId(), event.getTransactionId())
+                .forEach(orderInventory -> {
+                    var inventory = orderInventory.getInventory();
+                    inventory.setAvailable(orderInventory.getOldQuantity());
+                    inventoryRepository.save(inventory);
+                    log.info("Restored inventory for oder {} from {} to {}",
+                            event.getPayload().getId(),
+                            orderInventory.getNewQuantity(),
+                            inventory.getAvailable());
+                });
     }
 }
